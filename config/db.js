15493e4e4c9e3476c100df44a33e735e80908d2e
ezioguga <ezioguga@gmail.com>
2022-12-28 21:38:04 +0530

    added register service

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+};
+
+mongoose.set('strictQuery', true);
+
+module.exports = connectDB;
